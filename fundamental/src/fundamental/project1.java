4925f5f47647be73b8589087637fc987b9a205be
styli <styli@DESKTOP-VN4ALLG>
2019-06-28 11:29:53 +0530

    project

---
+++ fundamental/src/fundamental/project1.java
@@ -0,0 +1,15 @@
+package fundamental;
+
+public class project1 {
+	
+		public static void main(String[]args){
+		int a=Integer.parseInt(args[0]);
+		if(a%2==0)
+		System.out.println("Even");
+		else 
+		System.out.println("Odd");
+		}
+		
+
+
+}
